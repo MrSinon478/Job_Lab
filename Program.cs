6dbce7a9345827d8586045282408d990110d9408
MrSinon478 <144004045+MrSinon478@users.noreply.github.com>
2023-09-20 13:35:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("PR_4_part 5");
+Console.WriteLine();
+Console.WriteLine("Enter to name: ");
+string Name = Console.ReadLine();
+Console.WriteLine($"Hello {Name}");
