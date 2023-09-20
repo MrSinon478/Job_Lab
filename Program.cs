78a5d521fffd86566c2789f74090a93e71b2d0bc
MrSinon478 <144004045+MrSinon478@users.noreply.github.com>
2023-09-20 12:25:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 // --> using namespace std;
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("PR_4_part 5");
+Console.WriteLine();
+Console.WriteLine("Enter to name: ");
+string Name = Console.ReadLine();
+Console.WriteLine($"Hello {Name}");
